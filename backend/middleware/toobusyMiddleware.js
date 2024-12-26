83b791b815aaeaf72a3af864b0d29922ce3a554b
layumih <ayumi.cod06@gmail.com>
2024-12-26 14:20:55 -0300

    aumentando o limite de solicitações por IP

--- backend/middleware/toobusyMiddleware.js
+++ backend/middleware/toobusyMiddleware.js
@@ -1,5 +1,7 @@
 const toobusy = require('toobusy-js');
 
+toobusy.maxLag(1000)
+
 const toobusyMiddleware = (req, res, next) => {
   if (toobusy()) {
     res.status(503).send("Servidor muito ocupado. Tente novamente mais tarde.");
