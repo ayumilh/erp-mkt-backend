d0eada1e7b79198bd74656bb9d0500b6f6f6b646
layumih <ayumi.cod06@gmail.com>
2024-12-30 12:28:03 -0300

    alterando atributos da database e definindo valores padrões

--- backend/controllers/auth/mercadoLivre/productsMercadoLivre.js
+++ backend/controllers/auth/mercadoLivre/productsMercadoLivre.js
@@ -160,7 +160,7 @@ const mercadoLivreGetProductsSync = async (req, res) => {
             const marca = tokenData.attributes.find(attr => attr.id === "BRAND")?.value_name || "";
 
             // verificar se o produto tem GTIN
-            const gtinAttribute = tokenData.variations.attributes.find(attribute => attribute.id === "GTIN");
+            const gtinAttribute = tokenData.variations && tokenData.variations.length > 0 && tokenData.variations[0].attributes ? tokenData.variations[0].attributes.find(attribute => attribute.id === "GTIN") : null;
             const gtin = gtinAttribute ? gtinAttribute.value_name : '';
             console.log("GTIN:", gtin);
 
