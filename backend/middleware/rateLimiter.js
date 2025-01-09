cfe510dd103c3b314a4230725e38d761a9c838b0
layumih <ayumi.cod06@gmail.com>
2025-01-09 08:52:16 -0300

    configurando o proxy e o rateLimiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -3,7 +3,6 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
   max: 1000, // Limite de 1000 solicitações por IP por janela de 15 minutos
-  legacyHeaders: false,
   message: 'Muitas solicitações criadas a partir deste IP, por favor tente novamente após 15 minutos'
 });
 
